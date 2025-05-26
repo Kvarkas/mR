857e3ec0004c67e6bfbcd912a059c9632506c55e
Dimitrij <kvarkas@gmail.com>
2025-05-26 03:41:06 +0100

    fix the path

--- svelte.config.js
+++ svelte.config.js
@@ -5,7 +5,7 @@ const config = {
 	preprocess: vitePreprocess(),
 	kit: { adapter: adapter(),
 		paths: {
-			base: process.env.NODE_ENV === 'production' ? '/mR' : ''
+			base: process.env.NODE_ENV === 'production' ? '' : process.env.BASE_PATH
 		},
 	 }
 };
